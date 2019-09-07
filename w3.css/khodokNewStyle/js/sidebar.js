031c08fc8749120293de4590e3946eb700ec760c
Nadir Felder <nadir.felder1@gmail.com>
2019-09-07 19:20:55 +0200

    Yooohooooooooo matakaaatadada

--- w3.css/khodokNewStyle/js/sidebar.js
+++ w3.css/khodokNewStyle/js/sidebar.js
@@ -3,11 +3,16 @@ function w3_open() {
     document.getElementById("mySidebar").style.width = "15%";
     document.getElementById("mySidebar").style.display = "block";
     document.getElementById("myOverlay").style.display = "block";
-    document.getElementById("openNav").style.display = "none";
+    window.setTimeout(()=>{
+
+      document.getElementById("openNav").style.marginTop = "-150px";
+
+    }, 100);
+
 }
 function w3_close() {
     document.getElementById("main").style.marginLeft = "0%";
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
-    document.getElementById("openNav").style.display = "inline-block";
+    document.getElementById("openNav").style.marginTop = "0px";
 }
